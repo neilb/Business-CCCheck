9fb64fc565f16730c4cc10a586e71766e12939d8
Neil Bowers <neil@bowers.com>
2014-02-04 21:13:21 +0000

    Put correct GPL wording in license section

--- lib/Business/CCCheck.pm
+++ lib/Business/CCCheck.pm
@@ -4,7 +4,7 @@ use 5.006;
 use strict;
 use warnings;
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 use Business::CCCheck::CardID;
 
@@ -459,11 +459,9 @@ Validation criteria are:
 Copyright 2001 - 2011, Michael Robinton E<lt>michael@bizsystems.comE<gt>
 
 This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License (except as noted
-otherwise in individuals sub modules)  published by
-the Free Software Foundation; either version 2 of the License, or 
-(at your option) any later version, a copy of which is included with this
-distribution in the file "GPL".
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of 
